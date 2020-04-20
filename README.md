6e6b3010b7570058d226d012664606bca927df15
Geordon Worley <vadixidav@gmail.com>
2020-04-19 21:03:40 -0400

    initial definition of the types using silly alchemy terms

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # give
+
 A logic engine designed to give you things based on what you ask for
+
+## Implementation
+
+`give` only has one core primitive: types. Types in `give` can either be present or not present.
+
+In `give`, all operations happen through two types of conversions: implicit and explicit. An implicit conversion can happen without it being explicitly asked for by the user. Explicit conversions can only happen when a user specifies them.
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,33 @@
+use std::any::{TypeId, Any};
+use std::collections::HashMap;
+
+/// Transmutations require materials and produce a product.
+pub trait Transmutation {
+    fn materials(&self) -> Vec<TypeId>;
+    fn product(&self) -> TypeId;
+    fn transmute(&self, inputs: &[&dyn Any]) -> Box<dyn Any>;
+}
+
+pub struct Tome {
+    /// Transmutations observed to happen naturally are transcribed here.
+    /// The transmutations are organized by their products to discover new recepies.
+    notes: HashMap<TypeId, Box<dyn Transmutation>>,
+    /// Circles are inscribed here so that they be transcribed later.
+    circles: Vec<Box<dyn Transmutation>>,
+}
+
+impl Tome {
+    /// Inscribe a note about a natural transmutation into the tome.
+    pub fn note<T: Transmutation + 'static>(&mut self, note: T) {
+        self.notes.insert(note.product(), Box::new(note));
+    }
+
+    /// Inscribe a circle into the tome and give back the page of the inscription.
+    pub fn circle<T: Transmutation + 'static>(&mut self, circle: T) -> usize {
+        let page = self.circles.len();
+        self.circles.push(Box::new(circle));
+        page
+    }
+}
+
 
