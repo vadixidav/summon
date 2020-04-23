56efe5b89e7205df0b939955c812555940cec430
Geordon Worley <vadixidav@gmail.com>
2020-04-23 16:44:17 -0400

    implement summon

--- src/lib.rs
+++ src/lib.rs
@@ -25,22 +25,16 @@ impl Tome {
 
     /// Give me what I want.
     pub fn summon<T: 'static>(&self) -> Option<T> {
-        let recipe = self.research::<T>()?;
-        let mut materials: HashMap<TypeId, &dyn Any> = HashMap::new();
-        for (product, recipe) in recipe.transmutations {
-            let ingredients: Vec<&dyn Any> = recipe
-                .ingredients()
-                .iter()
-                .map(|ingredient| *materials.get(ingredient).unwrap())
-                .collect();
-            let product_box = recipe.transmute(&ingredients);
-        }
-        // Start by performing a depth-first search to aquire a recepie.
-        unimplemented!()
+        // Find a recipe to create the item. This may fail.
+        let recipe: Recipe = self.research::<T>()?;
+        // Perform the whole recipe. This cannot fail, excpet via panic.
+        let materials: Materials = recipe.transmutations.values().copied().collect();
+        // Drop all the intermediate materials to get only the desired one.
+        Some(materials.into_material::<T>())
     }
 
     /// Give me what I want, but inscribe the process so it can be more quickly executed in the future.
-    pub fn preserve<T>(&mut self) -> Option<T> {
+    pub fn preserve<T>(&self) -> Option<T> {
         unimplemented!()
     }
 
@@ -77,5 +71,53 @@ impl<'a> Recipe<'a> {
                 .or_insert_with(move || transmutation);
         }
         self
+    }
+}
+
+#[derive(Default)]
+struct Materials {
+    materials: HashMap<TypeId, Box<dyn Any>>,
+}
+
+impl Materials {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn get(&self, id: TypeId) -> Option<&dyn Any> {
+        self.materials.get(&id).map(|b| &**b)
+    }
+
+    fn apply(&mut self, recipe: &dyn Transmutation) {
+        let product_type = recipe.product();
+        let ingredients: Vec<&dyn Any> = recipe
+            .ingredients()
+            .iter()
+            .map(|&ingredient| self.get(ingredient).unwrap())
+            .collect();
+        let product = recipe.transmute(&ingredients);
+        self.materials.insert(product_type, product);
+    }
+
+    fn into_material<T: 'static>(mut self) -> T {
+        *self
+            .materials
+            .remove(&TypeId::of::<T>())
+            .unwrap()
+            .downcast::<T>()
+            .unwrap()
+    }
+}
+
+impl<'a> FromIterator<&'a dyn Transmutation> for Materials {
+    fn from_iter<T>(iter: T) -> Self
+    where
+        T: IntoIterator<Item = &'a dyn Transmutation>,
+    {
+        let mut materials = Self::new();
+        for recipe in iter {
+            materials.apply(recipe);
+        }
+        materials
     }
 }
